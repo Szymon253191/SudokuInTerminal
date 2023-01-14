f23492aa81b83863aac5ec60bd16ee5e011e05a5
Szymon <sedward154@gmail.com>
2023-01-14 16:25:37 +0100

    Updated printSudoku()
    Now there are numbers to help count rows and columns

--- src/SudokuGenerator.cpp
+++ src/SudokuGenerator.cpp
@@ -234,10 +234,11 @@ public:
 	// Print sudoku
 	void printSudoku()
 	{
-		cout << "  === === ===  === === ===  === === === " << endl;
+		cout << "     1   2   3    4   5   6    7   8   9  " << endl;
+		cout << "    === === ===  === === ===  === === === " << endl;
 		for (int i = 0; i < N; i++)
 		{
-			cout << "||";
+			cout << i+1 << " ||";
 			for (int j = 0; j < N; j++)
 			{
 				if (mat[i][j] != 0)
@@ -260,11 +261,11 @@ public:
 			cout << endl;
 			if (i % 3 == 2)
 			{
-				cout << "  === === ===  === === ===  === === === " << endl;
+				cout << "    === === ===  === === ===  === === === " << endl;
 			}
 			else
 			{
-				cout << "  --- --- ---  --- --- ---  --- --- --- " << endl;
+				cout << "    --- --- ---  --- --- ---  --- --- --- " << endl;
 			}
 		};
 	}
